3eea604138524818e12c783e7c841d7c26f1e813
Moshina <amoshina183@gmail.com>
2022-07-21 11:43:25 +0530

    semicolon added from local

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-        String name = "Moshina"
+        String name = "Moshina";
+//added again
         System.out.println("Name: "+name); 
     }
 }
