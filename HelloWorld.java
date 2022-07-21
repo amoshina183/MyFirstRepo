38e69b688ef65788c3246e83991640bba039d736
amoshina183 <83759091+amoshina183@users.noreply.github.com>
2022-07-21 11:29:11 +0530

    name added in code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        String name = "Moshina"
+        System.out.println("Name: "+name); 
     }
 }
